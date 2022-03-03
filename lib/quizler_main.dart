f163f7a9d20cf93845012733539babbe825e3710
aditi <aditi.singh@kelltontech.com>
2022-03-02 21:58:57 -0800

    first commit

---
+++ lib/quizler_main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scorekeeper = [];
+
+  List<String> questions = [
+    'India is a free Country.',
+    'Manmohan Singh is our prime Minister.',
+    'Mark Zuckberg is the founder of Facrbook.',
+  ];
+
+  List<bool> answers =[
+    true,
+    false,
+    true
+  ];
+
+  int questionNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions [questionNumber],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+
+                bool correctAnswer =answers[questionNumber];
+
+                if(correctAnswer ==true){
+                  print('user got it right!');
+                } else {
+                  print('user got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+                });
+                print(questionNumber);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                bool correctAnswer =answers[questionNumber];
+
+                if(correctAnswer ==true){
+                  print('user got it right!');
+                } else {
+                  print('user got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+                });
+                print(questionNumber);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children:scorekeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'India is a free Country'., true,
+question2: 'Manmohan Singh is our prime Minister.', false,
+question3: 'Mark Zuckberg is the founder of Facrbook.', true,
+*/
